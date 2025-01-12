356a6c53c495805483404cec90db3c1050c565e6
Kai Miyamoto <kaihogehoge@gmail.com>
2025-01-12 15:59:21 +0900

    feat: enumのプロパティ名を大文字のスネークケースに変更

--- src/v12/enum.ts
+++ src/v12/enum.ts
@@ -3,134 +3,131 @@
  */
 export const ContextType = {
   /** Content-centric context such as newsfeed, article, image gallery, video gallery */
-  Content: 1,
+  CONTENT: 1,
   /** Social-centric context such as social network feed, email, chat */
-  Social: 2,
+  SOCIAL: 2,
   /** Product context such as product listings, details, recommendations, reviews */
-  Product: 3,
+  PRODUCT: 3,
 } as const;
 
-export type ContextType = (typeof ContextType)[keyof typeof ContextType];
+export type ContextType = typeof ContextType[keyof typeof ContextType];
 
 /**
  * Context SubType IDs - Next-level context in which the ad appears
  */
 export const ContextSubType = {
   /** General or mixed content */
-  GeneralContent: 10,
+  GENERAL_CONTENT: 10,
   /** Primarily article content */
-  Article: 11,
+  ARTICLE: 11,
   /** Primarily video content */
-  Video: 12,
+  VIDEO: 12,
   /** Primarily audio content */
-  Audio: 13,
+  AUDIO: 13,
   /** Primarily image content */
-  Image: 14,
+  IMAGE: 14,
   /** User-generated content - forums, comments, etc */
-  UserGenerated: 15,
+  USER_GENERATED: 15,
   /** General social content such as a general social network */
-  GeneralSocial: 20,
+  GENERAL_SOCIAL: 20,
   /** Primarily email content */
-  Email: 21,
+  EMAIL: 21,
   /** Primarily chat/IM content */
-  Chat: 22,
+  CHAT: 22,
   /** Content focused on selling products */
-  ProductSales: 30,
+  PRODUCT_SALES: 30,
   /** Application store/marketplace */
-  AppStore: 31,
+  APP_STORE: 31,
   /** Product reviews site primarily */
-  Reviews: 32,
+  REVIEWS: 32,
 } as const;
 
-export type ContextSubType =
-  (typeof ContextSubType)[keyof typeof ContextSubType];
+export type ContextSubType = typeof ContextSubType[keyof typeof ContextSubType];
 
 /**
  * Placement Type IDs - The format of the ad you are purchasing
  */
 export const PlacementType = {
   /** In the feed of content */
-  FeedContent: 1,
+  FEED_CONTENT: 1,
   /** In the atomic unit of the content */
-  AtomicContent: 2,
+  ATOMIC_CONTENT: 2,
   /** Outside the core content */
-  OutsideCore: 3,
+  OUTSIDE_CORE: 3,
   /** Recommendation widget */
-  RecommendationWidget: 4,
+  RECOMMENDATION_WIDGET: 4,
 } as const;
 
-export type PlacementType = (typeof PlacementType)[keyof typeof PlacementType];
+export type PlacementType = typeof PlacementType[keyof typeof PlacementType];
 
 /**
  * Data Asset Types - Common asset element types of native advertising
  */
 export const DataAssetType = {
   /** Sponsored By message */
-  Sponsored: 1,
+  SPONSORED: 1,
   /** Descriptive text */
-  Desc: 2,
+  DESC: 2,
   /** Rating of the product */
-  Rating: 3,
+  RATING: 3,
   /** Number of social ratings or likes */
-  Likes: 4,
+  LIKES: 4,
   /** Number of downloads/installs */
-  Downloads: 5,
+  DOWNLOADS: 5,
   /** Price for product */
-  Price: 6,
+  PRICE: 6,
   /** Sale price */
-  SalePrice: 7,
+  SALE_PRICE: 7,
   /** Phone number */
-  Phone: 8,
+  PHONE: 8,
   /** Address */
-  Address: 9,
+  ADDRESS: 9,
   /** Additional descriptive text */
-  Desc2: 10,
+  DESC2: 10,
   /** Display URL */
-  DisplayUrl: 11,
+  DISPLAY_URL: 11,
   /** CTA description */
-  CtaText: 12,
+  CTA_TEXT: 12,
 } as const;
 
-export type DataAssetType = (typeof DataAssetType)[keyof typeof DataAssetType];
+export type DataAssetType = typeof DataAssetType[keyof typeof DataAssetType];
 
 /**
  * Image Asset Types - Common image asset element types
  */
 export const ImageAssetType = {
   /** Icon image for app icon, brand logo, or similar */
-  Icon: 1,
+  ICON: 1,
   /** Large image preview for the ad */
-  Main: 3,
+  MAIN: 3,
 } as const;
 
-export type ImageAssetType =
-  (typeof ImageAssetType)[keyof typeof ImageAssetType];
+export type ImageAssetType = typeof ImageAssetType[keyof typeof ImageAssetType];
 
 /**
  * Event Types - Types of events that can be tracked
  */
 export const EventType = {
   /** Impression */
-  Impression: 1,
+  IMPRESSION: 1,
   /** Visible impression using MRC definition at 50% in view for 1 second */
-  ViewableMrc50: 2,
+  VIEWABLE_MRC_50: 2,
   /** 100% in view for 1 second */
-  ViewableMrc100: 3,
+  VIEWABLE_MRC_100: 3,
   /** Visible impression for video using MRC definition at 50% in view for 2 seconds */
-  ViewableVideo50: 4,
+  VIEWABLE_VIDEO_50: 4,
 } as const;
 
-export type EventType = (typeof EventType)[keyof typeof EventType];
+export type EventType = typeof EventType[keyof typeof EventType];
 
 /**
  * Event Tracking Methods - Methods available for tracking events
  */
 export const EventTrackingMethod = {
   /** Image-pixel tracking */
-  Img: 1,
+  IMG: 1,
   /** Javascript-based tracking */
-  Js: 2,
+  JS: 2,
 } as const;
 
-export type EventTrackingMethod =
-  (typeof EventTrackingMethod)[keyof typeof EventTrackingMethod];
+export type EventTrackingMethod = typeof EventTrackingMethod[keyof typeof EventTrackingMethod];
